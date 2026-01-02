a2d1563679a31b899d4c0dc1821f0edaec0bca03
Andrei Amariei <Andrei.Amariei.1116@hotmail.com>
2026-01-02 21:18:41 +0200

    no cors error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://script.google.com/macros/s/AKfycbzCd4vrwi0IKtVY4P_59xro3S9DjtZGnTQ18lLxfh5_2_gmtHJE_Xemil94gB25fDp_5w/exec',
+  apiUrl: 'https://www.nibiru.fun/api',
   stripePaymentLink: 'https://buy.stripe.com/28E8wQgaX7yD0bp3Q75wI00'
 };

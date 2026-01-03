d51f5a49c403e81ad3fb1db2a6f5590d25156c7b
Andrei Amariei <Andrei.Amariei.1116@hotmail.com>
2026-01-03 21:17:04 +0200

    Asdasd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,21 @@
 import { Routes } from '@angular/router';
+
+// components
+import { HomeComponent } from './components/home/home';
+
+// lazy loaded components
 import { PaymentSuccessComponent } from './components/payment-success/payment-success';
 import { TicketListComponent } from './components/ticket-list/ticket-list';
 import { AddTicketFormComponent } from './components/add-ticket-form/add-ticket-form';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
     path: 'bilete',
-    component: TicketListComponent
+    loadComponent: () => import('./components/ticket-list/ticket-list').then(m => m.TicketListComponent)
   },
   {
     path: 'adauga-bilet',
@@ -17,7 +26,7 @@ export const routes: Routes = [
     loadComponent: () => import('./components/payment-success/payment-success').then(m => m.PaymentSuccessComponent)
   },
   {
-        path: '**',
-        redirectTo: 'bilete'
-    }
+    path: '**',
+    redirectTo: ''
+  }
 ];

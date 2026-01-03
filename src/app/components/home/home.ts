743fee3cd114150bae0b26f970258a064c699943
Andrei Amariei <Andrei.Amariei.1116@hotmail.com>
2026-01-03 22:39:21 +0200

    Asdasd

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 
+import { HlmButton } from '../../../../libs/ui/button/src/lib/hlm-button'
+
+
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    // directives
+    HlmButton
+  ],
   templateUrl: './home.html',
 })
 export class HomeComponent {}
